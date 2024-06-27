0ec14ac57494e0b1500ef25e09de08be67704e61
Gal machlev <galmachlev@gmail.com>
2024-06-27 13:37:53 +0300

    update

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from 'react';
 
 
 const Tab = createBottomTabNavigator();
